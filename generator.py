c5fa08eb438d5e98bb3e144d9aef7959688ab97f
asatovbobir <asatovbobir@gmail.com>
2025-02-26 18:28:45 -0500

    updates to generator

--- generator.py
+++ generator.py
@@ -10,15 +10,13 @@ from kittycad.models import (
     TextToCadCreateBody,
 )
 
-# Create our client.
 client = ClientFromEnv()
 
-# Prompt the API to generate a 3D model from text.
 response = create_text_to_cad.sync(
     client=client,
     output_format=FileExportFormat.STEP,
     body=TextToCadCreateBody(
-        prompt="Design a gear with 40 teeth",
+        prompt="Design a gear with 15 teeth",
     ),
 )
 
@@ -28,12 +26,9 @@ if isinstance(response, Error) or response is None:
 
 result: TextToCad = response
 
-# Polling to check if the task is complete
 while result.completed_at is None:
-    # Wait for 5 seconds before checking again
     time.sleep(5)
 
-    # Check the status of the task
     response = get_text_to_cad_model_for_user.sync(
         client=client,
         id=result.id,
@@ -46,20 +41,17 @@ while result.completed_at is None:
     result = response
 
 if result.status == ApiCallStatus.FAILED:
-    # Print out the error message
-    print(f"Text-to-CAD failed: {result.error}")
+    print(f"Da operation has failed: {result.error}")
 
 elif result.status == ApiCallStatus.COMPLETED:
     if result.outputs is None:
         print("Text-to-CAD completed but returned no files.")
         exit(0)
 
-    # Print out the names of the generated files
-    print(f"Text-to-CAD completed and returned {len(result.outputs)} files:")
+    print(f"Operation has been completed and returned {len(result.outputs)} files:")
     for name in result.outputs:
         print(f"  * {name}")
 
-    # Save the STEP data as text-to-cad-output.step
     final_result = result.outputs["source.step"]
     with open("text-to-cad-output.step", "w", encoding="utf-8") as output_file:
         output_file.write(final_result.decode("utf-8"))
